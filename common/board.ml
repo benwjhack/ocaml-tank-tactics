6dfdda46b949cfc6dd385858cdf738ba2f6a8d17
bhack <benwjhack@gmail.com>
2023-05-27 21:16:42 +0100

    added common board type

---
+++ common/board.ml
@@ -0,0 +1,15 @@
+open! Core
+
+module Tile = struct
+  type t = { type_ : int } [@@deriving sexp, bin_io, fields]
+
+  let type_of { type_; _ } = type_
+end
+
+type t = { tiles : Tile.t list list } [@@deriving sexp, bin_io, fields]
+
+let tile_at ~x ~y t =
+  let { tiles = xs; _ } = t in
+  let%bind.Option ys = List.nth xs x in
+  List.nth ys y
+;;
---
+++ common/board.mli
@@ -0,0 +1,12 @@
+open! Core
+
+module Tile : sig
+  type t [@@deriving sexp, bin_io]
+
+  (* bhack: types don't currently represent anything, just a placeholder *)
+  val type_of : t -> int
+end
+
+type t [@@deriving sexp, bin_io]
+
+val tile_at : x:int -> y:int -> t -> Tile.t option

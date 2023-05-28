03ae984c89752d6bc54ae92313deca097acf4a4b
bhack <benwjhack@gmail.com>
2023-05-28 05:19:42 +0100

    changed references of table in the client to map, updated rpc to use externals rather than individual components

--- common/room.mli
+++ common/room.mli
@@ -7,8 +7,15 @@ module Internals : sig
   val messages : t -> Message.t Queue.t
 end
 
+module Externals : sig
+  type t [@@deriving sexp_of, bin_io]
+
+  val board : t -> Board.t
+end
+
 type t
 
 val create : messages:Message.t Queue.t -> board:Board.t -> t
 val board : t -> Board.t
 val internals : t -> Internals.t
+val externals : t -> Externals.t
